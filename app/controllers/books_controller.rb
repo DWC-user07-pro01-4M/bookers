ba699e1ddcb2e5bff7f2bfc5b586f6d12697369d
DWC-user07- <124852092+DWC-user07->
2023-03-07 07:28:45 +0000

    [Add]バリデーション・コントローラ編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,14 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  # 保存機能
+  # 保存機能・バリデーション
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    render :new
+    end
   end
   # 投稿一覧
   def index
